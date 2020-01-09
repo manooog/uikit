32162bf1fd13125f56dd34cf7fa71029448206ae
阮细浩 <rxh12352@gmail.com>
2020-01-09 16:11:19 +0800

    修改一个引用错误

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /// <reference types="../types/index" />
 
 import { makeTime } from "./CRUD/common/baseFunction";
+import { cName } from "./utils";
 
 export { default as useListHook } from "./CRUD/listHook";
 export { default as useFormHook } from "./CRUD/formHook";
-export { cName } from "./utils";
 
-export const utils = { makeTime };
+export const utils = { makeTime, cName };
