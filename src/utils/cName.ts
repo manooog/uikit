0a4d18770361f93c235ef344366c2880e1c0f2bd
阮细浩 <rxh12352@gmail.com>
2020-02-21 21:01:25 +0800

    fix: cName函数修复类型声明和m函数默认值

--- src/utils/cName.ts
+++ src/utils/cName.ts
@@ -1,18 +1,38 @@
+import { uipre } from "../var/const";
+
+type modifier = (str?: string, flag?: Function | boolean) => result;
+
+type result = {
+  v: string;
+  m: modifier;
+};
+
+type thisObj = { v: string; combineVal: string[] };
+
+type addFun = (this: thisObj, str: string, septor: string) => result;
+
+type cName = (p: string) => (s?: string) => result;
+
 /**
- *
+ * # 定义bem中的b
  * var c = cName('test')
  * c().v => test
+ * # 定义bem中的e
  * c('b').v => test-b
  * c('b').m('active').v => test-b test-b--active
- * c('b').m('active').m('disabled').v => test-b test-b--active test-b--disabled
+ * # 如果bem任一部分过长，则需要使用下划线来隔断
+ * c('b').m('active').m('x_disabled').v => test-b test-b--active test-b--x_disabled
+ *
  */
 
-export const cName = (pre: string) => {
-  const add = function(
-    this: { v: string; combineVal: string[] },
-    str: string = "",
-    septor: string = "-"
-  ) {
+export const cName: cName = pre => {
+  pre = uipre + "_" + pre;
+  /**
+   * 追加className
+   * @param str 需要添加上去的字符串
+   * @param septor 默认为‘-’，也就是定义e部分
+   */
+  const add: addFun = function(str = "", septor = "-") {
     let val: string;
 
     if (septor === "-") {
@@ -22,20 +42,25 @@ export const cName = (pre: string) => {
     } else if (septor === "--") {
       if (str) this.combineVal.push(this.v + septor + str);
     }
-
+    const that = this;
     return {
-      m: (str?: string, flag: Function | boolean = true) => {
+      m: function(str) {
+        let flag: boolean | Function = true;
+
+        if (arguments.length > 1) flag = arguments[1];
+
         if (typeof flag === "function") {
           flag = !!flag();
         }
-        return add.call(this, flag ? str : "", "--");
+
+        return add.call(that, flag ? str : "", "--");
       },
       v: this.combineVal.join(" ")
     };
   };
 
-  return (str: string = "") => {
-    const obj = {
+  return (str = "") => {
+    const obj: thisObj = {
       v: "",
       combineVal: []
     };
